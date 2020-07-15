e1b0f401f8e5ba61edb6aeb4f0ae46829bdcd208
AndreyBob <54039246+AndreyBob@users.noreply.github.com>
2020-07-15 16:12:02 +0300

    Update Appliance.java

--- src/by/tc/task01/entity/Appliance.java
+++ src/by/tc/task01/entity/Appliance.java
@@ -1,13 +1,5 @@
 package by.tc.task01.entity;
 
-public class Appliance {
-    public Appliance() {
-    }
-
-    @Override
-    public String toString() {
-        return "Appliance{}";
-    }
-
-    // you may add your own code here
+public interface Appliance {
+   
 }
